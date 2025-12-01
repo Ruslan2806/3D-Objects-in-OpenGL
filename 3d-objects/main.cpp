1c616b00e4269a6747cd181c87d6fdeb30d45598
Ruslan Bichurin <115139612+Ruslan2806@users.noreply.github.com>
2025-12-01 19:10:50 +0300

    fix main

--- 3d-objects/main.cpp
+++ 3d-objects/main.cpp
@@ -1,12 +1,13 @@
-#include <GL/glew.h>
+#include <iostream>
 #include <SFML/Window.hpp>
-#include <SFML/OpenGL.hpp>
+#include <GL/glew.h>
 #include "shader.h"
 
 int main() {
     setlocale(LC_ALL, "ru");
 
     sf::Window window(sf::VideoMode(900, 900), "OpenGL Assignments", sf::Style::Default, sf::ContextSettings(32));
+
     window.setVerticalSyncEnabled(true);
     window.setActive(true);
 
@@ -26,11 +27,13 @@ int main() {
     std::cout << "Controls for Tetrahedron (task 1):" << std::endl;
     std::cout << "  Arrow keys - move X/Y" << std::endl;
     std::cout << "  W/S - move Z" << std::endl;
+    std::cout << "\nPress 1, 2, 3, 4 to switch assignments" << std::endl;
 
     Init();
 
     while (window.isOpen()) {
         sf::Event event;
+
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
@@ -40,19 +43,29 @@ int main() {
             }
             else if (event.type == sf::Event::KeyPressed) {
                 if (event.key.code == sf::Keyboard::Num1) {
+                    // Очистить текущее задание перед переключением
+                    Release();
                     currentAssignment = ASSIGNMENT_1;
+                    // Инициализировать новое задание
+                    Init();
                     std::cout << "Switched to Assignment 1: Gradient Tetrahedron" << std::endl;
                 }
                 else if (event.key.code == sf::Keyboard::Num2) {
+                    Release();
                     currentAssignment = ASSIGNMENT_2;
+                    Init();
                     std::cout << "Switched to Assignment 2" << std::endl;
                 }
                 else if (event.key.code == sf::Keyboard::Num3) {
+                    Release();
                     currentAssignment = ASSIGNMENT_3;
+                    Init();
                     std::cout << "Switched to Assignment 3" << std::endl;
                 }
                 else if (event.key.code == sf::Keyboard::Num4) {
+                    Release();
                     currentAssignment = ASSIGNMENT_4;
+                    Init();
                     std::cout << "Switched to Assignment 4" << std::endl;
                 }
             }
@@ -67,6 +80,5 @@ int main() {
     }
 
     Release();
-
     return 0;
 }
